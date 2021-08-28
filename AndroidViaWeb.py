a631d8c47809286cb12a318efc9df3732da738fc
hankin <niknah+github@gmail.com>
2021-08-28 13:26:53 +1000

    Update to python3

--- AndroidViaWeb.py
+++ AndroidViaWeb.py
@@ -2,14 +2,14 @@
 
 import sys
 import argparse
-import StringIO
 import re
 import os
+import io
 import time
 import urllib
-import urlparse
 import traceback
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer
+#from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from PIL import Image
 homeDir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(homeDir+'/AndroidViewClient/src')
@@ -35,9 +35,9 @@ class MyHandler(BaseHTTPRequestHandler):
             attempt += 5
             try:
                 self.do_GET2()
-            except Exception,e:
-                print 'error:'+str(e)
-                print 'error0:'+str(e.args[0])
+            except Exception as e:
+                print('error:'+str(e))
+                print('error0:'+str(e.args[0]))
                 traceback.print_exc()
                 print("reconnect, wait...")
                 time.sleep(5)
@@ -50,15 +50,15 @@ class MyHandler(BaseHTTPRequestHandler):
             self.defaultHeader('text/html')
             # Send the html message
             f = open(homeDir+"/index.html")
-            self.wfile.write(f.read())
+            self.wfile.write(bytes(f.read(),"utf-8"))
             f.close()
             return
         screenshotM = self.main.screenshotRe.match(self.path)
         if screenshotM is not None:
-            q = urlparse.parse_qs(screenshotM.group(1))
+            q = urllib.parse.parse_qs(screenshotM.group(1))
             imageFormat = q['imageFormat'][0]
             framebuffer = None
-            if q.has_key('framebuffer'):
+            if 'framebuffer' in q:
                 framebuffer = bool(int(q['framebuffer'][0]))
             img = self.main.screenshotPng(q['r'][0], imageFormat, framebuffer)
             self.defaultHeader('image/'+imageFormat)
@@ -69,31 +69,31 @@ class MyHandler(BaseHTTPRequestHandler):
         clickM = self.main.clickRe.match(self.path)
         if clickM is None and typeM is None:
             self.send_response(404)
-            self.wfile.write("404 You are lost")
+            self.wfile.write(bytes("404 You are lost","utf-8"))
             return
         if clickM is not None:
             if clickM.group(1) == 'touch':
                 self.defaultHeader('application/json')
                 self.main.touch(clickM.group(2).split(','))
-                self.wfile.write('{"ok":true}')
+                self.wfile.write(bytes('{"ok":true}',"utf-8"))
                 return
             elif clickM.group(1) == 'swipe':
                 self.defaultHeader('application/json')
                 self.main.swipe(clickM.group(2).split(','))
-                self.wfile.write('{"ok":true}')
+                self.wfile.write(bytes('{"ok":true}',"utf-8"))
                 return
         elif typeM is not None:
 
-            typeStr = urllib.unquote(typeM.group(2))
+            typeStr = urllib.parse.unquote(typeM.group(2))
             if typeM.group(1) == 'type':
                 self.defaultHeader('application/json')
                 self.main.device.type(typeStr)
-                self.wfile.write('{"ok":true}')
+                self.wfile.write(bytes('{"ok":true}',"utf-8"))
                 return
             elif typeM.group(1) == 'press':
                 self.defaultHeader('application/json')
                 self.main.device.press(typeStr)
-                self.wfile.write('{"ok":true}')
+                self.wfile.write(bytes('{"ok":true}',"utf-8"))
                 return
 
 
@@ -131,7 +131,8 @@ class AndroidViaWeb(object):
         # *** Bad: on some devices, this is compressing in png then uncompressing to PIL, then re-compressing again.
         # Need to reconnect after takeSnapshot...
         # https://github.com/dtmilano/AndroidViewClient/issues/46
-        image = self.device.takeSnapshot(reconnect=True, force_adb_framebuffer=framebuffer)
+#        image = self.device.takeSnapshot(reconnect=True, force_adb_framebuffer=framebuffer)
+        image = self.device.takeSnapshot(reconnect=True)
         rotate = int(rotate)
         transpose = None
         if rotate == 90:
@@ -142,9 +143,11 @@ class AndroidViaWeb(object):
             transpose = Image.ROTATE_270
         if transpose is not None:
             image = image.transpose(transpose)
-        output = StringIO.StringIO()
+        output = io.BytesIO()
         if(imageFormat =='JPEG'):
-            image.save(output, imageFormat, quality=10)
+            imageRGB = Image.new("RGB", image.size, (0,0,0))
+            imageRGB.paste(image, mask = image.split()[3])
+            imageRGB.save(output, imageFormat, quality=10)
         else:
             image.save(output, imageFormat)
         contents = output.getvalue()
@@ -169,13 +172,13 @@ class AndroidViaWeb(object):
             #incoming request
             server = HTTPServer(('', self.port), MyHandler)
             server.androidViaWeb = self
-            print 'Started httpserver on port ', self.port
+            print('Started httpserver on port ', self.port)
 
             #Wait forever for incoming htto requests
             server.serve_forever()
 
-        except KeyboardInterrupt:
-            print '^C received, shutting down the web server'
+        except KeyboardInterrupt as e:
+            print('^C received, shutting down the web server')
             server.socket.close()
 
 
